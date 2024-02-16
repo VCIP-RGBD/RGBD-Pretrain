89d420493b5573e61fa4935af5876f16672445e5
Bo-Wen Yin <52337138+yinbow@users.noreply.github.com>
2024-02-16 17:33:04 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,7 +1,7 @@
 MODEL=DFormer_Small # drop-path 0.1, 0.1, 0.15, 0.2 for T,S,B,L
 
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 PORT=31929 bash distributed_train.sh 8 \
-		/root/siton-gpfs-pubdata/imagenet/ \
+		/datasets/ImageNet/ \
 	  	--model $MODEL \
 		-b 128 \
 		--epochs 300 \
